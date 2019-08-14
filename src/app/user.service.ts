840c3ead9135bab9dd90b0917a310ca5f425507a
kschwartz439 <kschwartz439@gmail.com>
2019-08-13 21:22:20 -0500

    checking connection

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -11,6 +11,6 @@ export class UserService{
     constructor(private httpClient:HttpClient){}
 
     getUsers(): Observable<User[]>{
-        return this.httpClient.get<User[]>('http://localhost:8181/users')
+        return this.httpClient.get<User[]>('http://18.191.142.3:8181')
     }
 }
