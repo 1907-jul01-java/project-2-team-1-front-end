7f4fdf1c0b6b639cfd1ae1b05a1e56817c12e080
Elia Rice <eebrwriting@gmail.com>
2019-08-21 22:14:06 -0500

    CSS Changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { TopService } from '../top.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	top;
+	top: [];
 
   constructor(private topService : TopService) { }
 
